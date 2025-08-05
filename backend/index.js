4a7bb6616a9df171bc0d3a870483e0e9d80bacc1
deepak445566 <deepakpandey1212d@gmail.com>
2025-08-05 10:47:30 +0530

    Updated backend: login logic or CORS settings or other changes

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+
+  "https://frontend-zerodha-clone.netlify.app",
+  "https://dashboard-zerodha-clone.netlify.app"
+];
+
 
 app.use(cors({
   origin: function (origin, callback) {
